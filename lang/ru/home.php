b29df3df5cf7293032ca01f4575782fca3a16882
Sergey <sergey@otryvanov.me>
2014-09-29 00:32:28 +0400

    Update home.php

--- lang/ru/home.php
+++ lang/ru/home.php
@@ -1,5 +1,6 @@
 <?php
 
+    $g_lang['welcome'] = 'Welcome to Micron!';
     $g_lang['message'] = 'Дорогой пользователь, добро пожаловать в микрон - самый простой пхп фреймворк.';
     $g_lang['build']   = 'Build %s';
 ?>
